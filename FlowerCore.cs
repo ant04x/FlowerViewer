c0a945da3acd883bae5984cfc1f1f347f7bd47c9
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-03-11 15:17:06 +0100

    Incorporados últimos botones y estructura de interacción.
    Se han establecido la estructura de clases de interacción con la interfaz, y se han incorporado los últimos botones de la interfaz.

---
+++ FlowerCore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FlowerViewer
+{
+    class FlowerCore
+    {
+        public static void Process(MainWindow mw)
+        {
+            /*----------------NAVEGATION----------------*/
+            mw.navBtnBack.Click += (o, i) =>
+            {
+                
+            };
+
+            mw.navBtnNext.Click += (o, i) =>
+            {
+
+            };
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,12 @@ using System.Windows.Shapes;
 
 namespace FlowerViewer
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            FlowerCore.Process(this);
         }
     }
 }
