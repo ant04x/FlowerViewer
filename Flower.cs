6a46bc75d48a6dc02ea3baa83b85e833da4ecfc5
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-03-11 14:54:12 +0100

    Agregada clase Flower
    Se ha agregado la clase Flower para cargar los datos sobre esta clase de objetos.

---
+++ Flower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerViewer
+{
+    class Flower
+    {
+        string name;
+
+        enum Color
+        {
+            Blanco,
+            Rojo,
+            Amarillo,
+            Morado,
+            Azul
+        }
+
+        string description;
+
+        byte[] image;
+    }
+}
